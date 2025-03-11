9942b67d8da4c1226b7bd733aafcf6247d15a5b0
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-03-11 12:23:44 +0600

    solution added

---
+++ SimpleFunction.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+// MARK: allSatisfy
+func collectionIsDivisibleBy2(arr: Array<Int>) -> Bool {
+    guard !arr.isEmpty else { return false }
+    return arr.allSatisfy({$0 % 2 == 0})
+}
+
+// MARK: max
+func findTheLargestNumber(from arr: Array<Int>) -> Int {
+    guard !arr.isEmpty else { return 0 }
+    return arr.max() ?? 0
+}
+
+// MARK: min
+func findTheSmallestNumber(from arr: Array<Int>) -> Int {
+    guard !arr.isEmpty else { return 0 }
+    return arr.min() ?? 0
+}
+
+/**
+ Input: [1,2,3,4,5]
+ Output: 15
+ */
+// MARK: Reduce
+func getArraySum(arr: Array<Int>) -> Int {
+    guard !arr.isEmpty else { return 0 }
+    return arr.reduce(0, {$0 + $1})
+}
+
+
+func doesContain(elementWithName name: String, inCollection names: Array<String>) -> Bool {
+    guard !names.isEmpty else {
+        return false
+    }    
+    return names.contains(where: {$0.caseInsensitiveCompare(name) == .orderedSame})
+}
